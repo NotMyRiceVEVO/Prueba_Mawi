1d3a551f036cacf88b16bac208be1968b321682d
NotMyRice <m.lomeoropeza@gmail.com>
2026-09-07 22:30:13 -0600

    Confirmacion de validaciones de datos en registros de tiempo

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -4,6 +4,7 @@ use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
 $routes->get('/', 'Home::index');
+$routes->get('proyectos/(:num)/resumen', 'Proyectos::resumen/$1');
 $routes->resource('proyectos', ['controller' => 'Proyectos']);
 $routes->resource('empleados', ['controller' => 'Empleados']);
 $routes->resource('registros-tiempo', ['controller' => 'RegistrosTiempo']);
--- backend/app/Controllers/Proyectos.php
+++ backend/app/Controllers/Proyectos.php
@@ -70,4 +70,38 @@ class Proyectos extends ResourceController
 
         return $this->respondDeleted(['message' => 'Proyecto eliminado correctamente.']);
     }
+
+    public function resumen($id = null)
+    {
+        $proyecto = $this->model->find($id);
+
+        if (!$proyecto) {
+            return $this->failNotFound('Proyecto no encontrado.');
+        }
+
+        $registrosModel = new \App\Models\RegistrosTiempoModel();
+
+        $registros = $registrosModel
+            ->select('registros_tiempo.horas, empleados.tarifa_hora')
+            ->join('empleados', 'empleados.id = registros_tiempo.empleado_id')
+            ->where('registros_tiempo.proyecto_id', $id)
+            ->findAll();
+
+        $horasTotales = 0;
+        $costoTotal   = 0;
+
+        foreach ($registros as $registro) {
+            $horasTotales += $registro['horas'];
+            $costoTotal   += $registro['horas'] * $registro['tarifa_hora'];
+        }
+
+        return $this->respond([
+            'proyecto_id'      => (int) $id,
+            'nombre'           => $proyecto['nombre'],
+            'presupuesto'      => (float) $proyecto['presupuesto'],
+            'horas_totales'    => (float) $horasTotales,
+            'costo_total'      => (float) $costoTotal,
+            'excede_presupuesto' => $costoTotal > $proyecto['presupuesto'],
+        ]);
+    }
 }
